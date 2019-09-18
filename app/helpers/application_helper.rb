d7aa053b09327c511cb20e60e481e071c9d50fa2
Phil Grant <philfromcalgary@gmail.com>
2019-09-18 09:56:33 -0700

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,3 +59,16 @@ module ApplicationHelper
     "active" if current_page? path
   end
 end
+
+def alerts
+  alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+  if alert
+     alert_generator alert
+  end
+end
+
+def alert_generator msg
+  js add_gritter(msg, title: "Philip Grant Portfolio", sticky: false, time: 4000)
+end
+
