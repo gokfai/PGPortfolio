905c7e3ec2a3f2379d51f66c9d479443290eeac5
Phil Grant <philfromcalgary@gmail.com>
2019-12-02 12:45:02 -0800

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://vaspec-pgportfolio.herokuapp.com', 'http://vaspec-pgportfolio.herokuapp.com']
+  config.action_cable.url = "wss://vaspec-pgportfolio.herokuapp.com/cable"
 end
